02a385b761848fe9b4647b9e2fbe3fb23d3162f2
JoaoGabrielOliveira <joaogabrieloliveira81@gmail.com>
2020-05-04 09:44:26 -0300

    Hello World com React

---
+++ app/javascript/app.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react'
+//import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-bootstrap/dist/bootstrap.min.css'
+import Button from 'react-bootstrap/Button'
+
+
+const App = () => {
+    return(
+        <Fragment>
+            <Button color='primary'> Hello World </Button>
+        </Fragment>
+    )
+}
+
+export default App;
