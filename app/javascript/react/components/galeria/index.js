1332bfab57bd90f02ef4c25075a83fbdd482a53b
JoaoGabrielOliveira <joaogabrieloliveira81@gmail.com>
2020-05-08 16:26:50 -0300

    Ajustamentos de Galeria

--- app/javascript/react/components/galeria/index.js
+++ app/javascript/react/components/galeria/index.js
@@ -4,17 +4,40 @@ import React from 'react'
 /*
     Titulo:string
     Pagination:bool
+
+    
 */
 const Galeria = (props) =>
 {
-    let Hearder = () =>
-    {
+    return(
+        <div className='Galeria'>
+            <Header>
+                <h1>{props.title}</h1>
+            </Header>
+            {props.children}
+        </div>
+    );
+}
+
+const Header = (props) => 
+{
         return(
             <header>
-                <h1>{props.title}</h1>
+                {props.children}
             </header>
         );
-    }
+}
+
+const Galeria_Imagens = (props) => 
+{
+    <div className='Galeria-Imagens'>
+        {props.children}
+    </div>
+}
+
+const Galeria_Item = (props) =>
+{
+    <img src={props.src}/>
 }
 
 export default Galeria;
--- app/javascript/react/screens/gallery/index.js
+++ app/javascript/react/screens/gallery/index.js
@@ -5,7 +5,13 @@ import './index.css';
 
 const GalleryScreen  = () => {
         return (
-                <Galeria.Header />
+                <Galeria title="Galeria">                
+                        <Galeria_Imagens>
+                                <Galeria_Item src='https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/4bc29492520453.5e4d4be7c884f.jpg' />
+                                <Galeria_Item src='https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/4bc29492520453.5e4d4be7c884f.jpg' />
+                                <Galeria_Item src='https://mir-s3-cdn-cf.behance.net/project_modules/1400_opt_1/4bc29492520453.5e4d4be7c884f.jpg' />
+                        </Galeria_Imagens>
+                </Galeria>
         );
 }
  
